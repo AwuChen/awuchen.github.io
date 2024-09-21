064753a0de14124a5efbbc121cc3cc252ed57669
Awu Chen <awu.mxr@gmail.com>
2024-09-21 12:32:12 -0700

    created a production build

---
+++ build/precache-manifest.9e23010dfbe4bca3bb5b74e26689c7b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e07f3c22de239b4ca354ee735d33f005",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b1e2bccbf403f30b196",
+    "url": "/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "c98ba2894aebf0bd2f48",
+    "url": "/static/js/2.74acac1e.chunk.js"
+  },
+  {
+    "revision": "dad2f28e977fd3345e1525c31e9ba66e",
+    "url": "/static/js/2.74acac1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b1e2bccbf403f30b196",
+    "url": "/static/js/main.87ade3d0.chunk.js"
+  },
+  {
+    "revision": "6e997a26d84ead5111da",
+    "url": "/static/js/runtime-main.79066f12.js"
+  }
+]);
